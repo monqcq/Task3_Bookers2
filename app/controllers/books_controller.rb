7068021b5649e377a239be1be85dd68326cdb97f
monqcq <takuyaaaan23@gmail.com>
2021-03-17 11:59:15 +0000

    [add] 一覧ページの左側

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,19 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all.order("id ASC")
+    @book = Book.new
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/:id'
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
