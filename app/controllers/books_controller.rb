30fc9d82db76cbb60adaed4899bf20dcc5a7ce57
monqcq <takuyaaaan23@gmail.com>
2021-03-17 15:21:55 +0000

    [add] book一覧

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,17 +3,17 @@ class BooksController < ApplicationController
     @books = Book.all.order("id ASC")
     @book = Book.new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
     redirect_to '/books/:id'
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
