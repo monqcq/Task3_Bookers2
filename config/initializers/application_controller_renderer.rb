63f17db02f661754a6f575f12f22a31079c1abe3
monqcq <takuyaaaan23@gmail.com>
2021-03-18 12:51:12 +0000

    [finish] user編集ページ

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'be5cd0513026ed49e86b578773cc0fb867f0f63a56cfb4677be083b08ea956a54de62f888baa2e0e5fb9ad68fc26c8797e39964eb03cbd15720dec9f6f82d775'
