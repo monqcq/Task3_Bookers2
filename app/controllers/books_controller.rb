c2eef850490c4e434247cab7e16a47d1ba2b5cf1
monqcq <takuyaaaan23@gmail.com>
2021-03-18 07:05:48 +0000

    [finish] book詳細

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,18 @@ class BooksController < ApplicationController
     book = Book.new(book_params)
     book.user_id = current_user.id
     book.save
-    redirect_to '/books/:id'
+    redirect_to book_path(book.id)
+  end
+  
+  def show
+    @book = Book.new
+    @bookDetail = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
