42c4273623ce6b8e450fbd6f0b4ec5de036d8ef3
monqcq <takuyaaaan23@gmail.com>
2021-03-18 08:50:07 +0000

    [finish] book編集画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
-  
+
+  def index
+    @book = Book.new
+    @users = User.page(params[:page]).reverse_order
+  end
+
+  def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books.page(params[:page]).reverse_order
+  end
+
 end
