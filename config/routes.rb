0f2abf72fe8bae75db3063962526fdadd76d508f
monqcq <takuyaaaan23@gmail.com>
2021-04-01 12:04:58 +0000

    [add] いいね機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about', to: 'homes#about'
-  resources :books
   resources :users, only: [:index, :show, :edit, :update]
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
