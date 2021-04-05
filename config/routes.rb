aa4c138920a8cdf0f34d26d221cfb3698dc969b9
monqcq <takuyaaaan23@gmail.com>
2021-04-05 08:13:20 +0000

    [add] followerボタン

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about', to: 'homes#about'
   resources :users, only: [:index, :show, :edit, :update]
+  resources :relationships, only: [:create, :destroy]
+  
+  
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
